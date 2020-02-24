5cd03df9d06ecc1ab64323af00044717f91353a8
Ahnaf Shahriar Abir <abir3577189@gmail.com>
2020-02-24 16:38:41 +1100

    Updates

---
+++ precache-manifest.313669bb5d50b10943851dbe14ca5a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426d4413822b592174f4e44577c6ffa2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e6549f01d835ea56f474",
+    "url": "/monsters/static/css/main.39c561e5.chunk.css"
+  },
+  {
+    "revision": "4c7c32feb10d991b81c7",
+    "url": "/monsters/static/js/2.75f2c6d9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.75f2c6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6549f01d835ea56f474",
+    "url": "/monsters/static/js/main.5bdbc6e1.chunk.js"
+  },
+  {
+    "revision": "6b1c49df7b8ad8f96b8b",
+    "url": "/monsters/static/js/runtime-main.403fa523.js"
+  }
+]);
